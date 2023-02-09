3095b37a7b7e0e0d3d16bc6a2584ea9fcc2a0ea7
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-02-09 19:25:38 +0530

    Added : Validate the User Entry using JUnit Test

--- src/main/java/com/bridgelabz/regexpattern/UserRegistration.java
+++ src/main/java/com/bridgelabz/regexpattern/UserRegistration.java
@@ -1,26 +1,31 @@
 package com.bridgelabz.regexpattern;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	
-public static void validate(String email) {
-		
-		String emailPattern = "^[a-z].[0-9]?.*[\\.\\+\\@\\-][a-z]?.*[a-z_\\.\\,]$";
-		boolean id = false;
-		id = Pattern.matches(emailPattern, email);
-		if(id == true)
-			System.out.println("Email ID : "+email+"\nEmail ID is Valid");
-		else
-			System.out.println("Email ID is Invalid Format !!!");
-	}
 
-	public static void main(String[] args) {
+	public boolean FirstName(String firstName, String namePattern) {
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the Email ID : ");
-		String email = scan.next();
-		validate(email);
+		return Pattern.matches(namePattern, firstName);
+	}
+	
+	public boolean LastName(String firstName, String namePattern) {
+		
+		return Pattern.matches(namePattern, firstName);
+	}
+	
+	public boolean Email(String email, String emailPattern) {
+		
+		return Pattern.matches(emailPattern, email);
+	}
+	
+	public boolean PhoneNumber(CharSequence phoneNumber, String numberPattern) {
+		
+		return Pattern.matches(numberPattern, phoneNumber);
+	}
+	
+	public boolean Password(String password, String passwordPattern) {
+		
+		return Pattern.matches(passwordPattern, password);
 	}
 }
